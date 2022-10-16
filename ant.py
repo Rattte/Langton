8535c281eddd9ff632d4d77b7c6435b747dee009
MattW <email4mpw@gmail.com>
2022-10-16 18:07:14 +1100

    more efficeient, more patterns

--- ant.py
+++ ant.py
@@ -5,7 +5,13 @@ import time
   
 def langton():
     
-    size = 1200
+    
+    size = 1000
+    #bigger number = faster
+    updates = 10000
+    
+    #less ants = faster
+    Antcount = 1
     
     
   
@@ -20,11 +26,13 @@ def langton():
     LR = [("black","R"),("white","L")]
     LLRR = [("blue","L"), ("pink","R"), ("black","R"),("white","L")]
     LLRRRLRLRLLR = [("blue","L"), ("pink","R"), ("green","R"), ("azure","R"), ("cyan","L"), ("beige","R"), ("olive","L"), ("gold","R"), ("goldenrod","L"), ("yellow","L"), ("black","R"),("white","L")]
-  
-    
-    
+    LRL = [("black","R"),("blue","L"),("white","L")]
+    LLRL = [("black","R"),("green","L"),("blue","L"),("white","L")]
+    LLRLR = [("goldenrod","L"), ("black","R"),("green","L"),("blue","R"),("white","L")]
+    RRLLLRLLLRRR = [("blue","R"), ("pink","L"), ("green","L"), ("azure","L"), ("cyan","R"), ("beige","L"), ("olive","L"), ("gold","L"), ("goldenrod","R"), ("yellow","R"), ("black","R"),("white","R")]
+    turtle.delay(0)
     ants = []
-    for i in range(1):
+    for i in range(Antcount):
         ants.append(turtle.Turtle())
         ants[i].shape('square')    
       
@@ -32,18 +40,20 @@ def langton():
         ants[i].shapesize(antsize)
         # speed of the ant
         ants[i].speed(0)     
+        ants[i].ht()
     step = antsize*20   
     #disable turtle update
     turtle.tracer(0,0)    
     # gives the coordinate of the ant                
-                  
+    antcolour = RRLLLRLLLRRR           
     count = 0
-    antcolour = LLRR
+
     totalcount = 0
     while len(ants) > 0:
-        totalcount +=1
+        
         #time.sleep(1)
         for ant in ants:
+            totalcount +=1
             
             pos = coordinate(ant)    
             count+=1          
@@ -62,7 +72,6 @@ def langton():
                 #find index of colour
                 for i in antcolour:
                     
-                    
                     if i[0] == maps[pos]:
                         ind = antcolour.index(i)+1 
                         if antcolour.index(i)>=len(antcolour):
@@ -77,14 +86,16 @@ def langton():
                         ant.forward(step)
                         pos=coordinate(ant)
                         break
-            if count >= 20:
+            if count >= updates:
                 turtle.update()
                 count=0
             if abs(ant.xcor()) >= size/2 or abs(ant.ycor()) >= size/2:
                 ants.pop(ants.index(ant))
+                print("ant finished after ",totalcount," steps")
             if totalcount %1000 == 0:
                 print(totalcount)
     while True:
+        turtle.update()
         window.exitonclick()
     
         
